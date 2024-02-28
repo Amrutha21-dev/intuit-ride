82464662fc125da688681da814e23230b85d6b11
Amrutha <amruthavarshini931@gmail.com>
2024-02-29 00:40:13 +0530

    Initial Commit

--- src/main/java/com/intuit/demo/RideApplication.java
+++ src/main/java/com/intuit/demo/RideApplication.java
@@ -2,9 +2,11 @@ package com.intuit.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 //@EnableKafka
+@EnableTransactionManagement
 public class RideApplication {
 
 	public static void main(String[] args) {
